a1332e2677cb16215692305244640519259b08a3
Preston Johnstone <pj1137@students.cs.ubc.ca>
2026-05-26 18:26:34 -0700

    Updated Scraper

--- Web Scraping Test - INaturalist/main.py
+++ Web Scraping Test - INaturalist/main.py
@@ -3,9 +3,9 @@ import requests
 api = "https://api.inaturalist.org/v2/observations"
 
 params = {
-    "taxon_name": "Coleoptera",
+    "taxon_id": 47208,
     "quality_grade": "research",
-    "per_page": 5,
+    "per_page": 10,
     "fields": "photos.url"
 }
 
@@ -13,8 +13,10 @@ response = requests.get(api, params=params)
 
 data = response.json()
 
+amount = 1
 for obs in data["results"]:
-    photo = obs["photos"][0]["url"]
-    photo = photo.replace("square", "small")
-    print(photo)
-
+    print(f"Entry {amount}:")
+    for photo in obs["photos"]:
+        photo = photo["url"].replace("square", "large")
+        print(photo)
+    amount += 1
